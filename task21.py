4f1116e85fe3ff25c7b12389b7c4473f8133c526
kanat nadyrbekov <kanatnadyrbekov@gmail.com>
2019-12-30 09:50:09 +0600

    Ch1Part Task 21

---
+++ task21.py
@@ -0,0 +1,16 @@
+# Напишите функцию которая будет суммировать введенные три случайные цифры.
+
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+
+# print(f"result is : {a+b+c}")
+
+def add(a, b, c):
+    result = (a + b + c)
+    return result
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+    
+print(add(a = int(input("")), b = int(input("b =")), c = int(input("c="))))
